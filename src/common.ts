54a157e7d357a29159b781d01c28b1c0cb2776aa
skid <skid@kinkpad.local>
2026-05-14 17:16:06 +0200

    updated ticks per sec

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,4 @@
-export const TICK_FREQUENCY = 60; // ticks per second
+export const TICK_FREQUENCY = 30; // ticks per second
 export const PERSON_W = 40;
 export const PERSON_H = 120;
 export const EPSILON = 0.0001;
