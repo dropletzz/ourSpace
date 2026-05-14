95d52ee2c360adb8d7b8b8db55410c4faeefc9c7
skid <skid@kinkpad.local>
2026-05-14 16:54:59 +0200

    restored zoom

--- src/client/user-input.ts
+++ src/client/user-input.ts
@@ -39,9 +39,8 @@ export class UserInput {
             else if (event.code == "KeyD") this.right = true;
 
             this.updateMoveDirections();
-
-            this.updateMoveDirections();
         });
+
         document.addEventListener("keyup", (event) => {
             if (event.code == "KeyW") this.up = false;
             else if (event.code == "KeyA") this.left = false;
@@ -71,6 +70,18 @@ export class UserInput {
 
         this.canvas.addEventListener('mousedown', (event) => { if (event.button === 0) this.mouseLeftPressed = true; });
         this.canvas.addEventListener('mouseup', (event) => { if (event.button === 0) this.mouseLeftPressed = false; });
+
+        window.addEventListener('wheel', (event) => {
+            event.preventDefault();
+
+            if (event.deltaY > 0) {
+                this.zoom *= (1 - ZOOM_SPEED);
+            } else {
+                this.zoom *= (1 + ZOOM_SPEED);
+            }
+
+            this.zoom = Math.min(Math.max(ZOOM_MIN, this.zoom), ZOOM_MAX);
+        }, { passive: false });
     }
 
     updateMoveDirections() {
